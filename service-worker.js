2940b8818beb79f16666fb6ccb593b674d943b6b
Arceniss <arceniss@gmail.com>
2025-03-13 20:44:26 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,31 @@
 self.addEventListener('install', event => {
+    // Instalación del Service Worker
     event.waitUntil(
         caches.open('mi-cache').then(cache => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/manifest.json',
-                '/icon-192x192.png',
-                '/icon-512x512.png'
+                '/Prueba1-de-PWA/',
+                '/Prueba1-de-PWA/index.html',
+                '/Prueba1-de-PWA/manifest.json',
+                '/Prueba1-de-PWA/icon-192x192.png',
+                '/Prueba1-de-PWA/icon-512x512.png'
             ]);
+        }).catch(error => {
+            console.error('Fallo al cargar caché:', error);
         })
     );
 });
 
 self.addEventListener('fetch', event => {
+    // Intercepta las solicitudes y responde con recursos en caché o del servidor
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
+        }).catch(error => {
+            console.error('Error en fetch:', error);
         })
     );
 });
+
 
 
 /*
